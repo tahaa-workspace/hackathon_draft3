8893cdc109b3e30568059c5a52b6f5b8d8d931f0
Tahaa Mandsorwala <tahaa.workspace@gmail.com>
2026-08-29 14:05:44 +0530

    Accept Aadhaar file during registration

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { register, login, changePassword } from '../controllers/authController.js';
 import protect from '../middleware/authMiddleware.js';
+import upload from '../middleware/uploadMiddleware.js';
 
 const router = Router();
 
-router.post('/register', register);
+router.post('/register', upload.single('aadhaar'), register);
 router.post('/login', login);
 router.post('/change-password', protect, changePassword);
 
