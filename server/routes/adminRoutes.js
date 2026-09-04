a3f1a0190a4fb58debefe1016865a19f06f82475
Tahaa Mandsorwala <tahaa.workspace@gmail.com>
2026-09-04 22:36:45 +0530

    Add lawyer credential review route

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,6 +4,7 @@ import {
   listUsers,
   updateUserStatus,
   getAadhaarReviewUrl,
+  getLawyerCredentialReviewUrl,
   approveUser,
   rejectUser,
 } from '../controllers/adminController.js';
@@ -16,6 +17,7 @@ router.get('/registrations', protect, authorize('ADMIN'), listPendingRegistratio
 router.get('/users', protect, authorize('ADMIN'), listUsers);
 router.put('/users/:id/status', protect, authorize('ADMIN'), updateUserStatus);
 router.get('/users/:id/aadhaar', protect, authorize('ADMIN'), getAadhaarReviewUrl);
+router.get('/users/:id/lawyer-credential', protect, authorize('ADMIN'), getLawyerCredentialReviewUrl);
 router.put('/users/:id/approve', protect, authorize('ADMIN'), approveUser);
 router.put('/users/:id/reject', protect, authorize('ADMIN'), rejectUser);
 
